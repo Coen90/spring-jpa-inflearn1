2e877e843abc242ee6e31a73ef9cda44fc2a0c70
Hyuntae <bht9011@gmail.com>
2022-04-18 23:15:54 +0900

    jpashop domain model and table architect

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,10 +1,13 @@
 package jpabook.jpashop;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,14 +19,21 @@ class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
+    @Transactional
+    @Rollback(value = false)
     public void testMember() throws Exception {
         // given
-       Member member = new Member();
-       member.setUsername("memberA");
-        // when
-12:12
-        Long save = memberRepository.save(member);
-        // then
+        Member member = new Member();
+        member.setUsername("memberA");
 
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member: " + (findMember == member));
     }
 }
